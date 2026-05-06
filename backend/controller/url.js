c3f05814237ff662cae598709a95ad501305fde9
sn1638 <sachin111mrj@gmail.com>
2026-05-06 22:43:57 +0530

    included a feature for checking unique short url in the database

--- backend/controller/url.js
+++ backend/controller/url.js
@@ -1,18 +1,26 @@
 import shortid from 'shortid'
 import { URL} from './../models/url.js' 
 export async function handleshorturl(req,res){
-try {
-        const body=req.body
-        if(!body || !body.url)   return res.status(400).json({error:'url is required'})
-        const shortId=shortid.generate()
-        await URL.create({
-            shortId,
-            redirecturl:body.url,
-            visithistory:[],
-        })
-        return res.json({id:shortId})
-} catch (error) {
-    console.error(error.message,"error creating short url")
-    res.status(500).json({error:'internal server error'})
-}
+    try {
+            const body=req.body
+            if(!body || !body.url)   return res.status(400).json({error:'url is required'})
+            
+            let exists=true;
+            let shortId;
+            while (exists) {
+                shortId=shortid.generate()
+                const check = await URL.findOne({ shortId });
+                if (!check) exists = false;
+            }
+
+            await URL.create({
+                shortId,
+                redirecturl:body.url,
+                visithistory:[],
+            })
+            return res.json({id:shortId})
+    } catch (error) {
+        console.error(error.message,"error creating short url")
+        res.status(500).json({error:'internal server error'})
+    }
 }
